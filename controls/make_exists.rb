e014f1232f232d19858ed9ae9db52c4d0c502f9f
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 17:25:52 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/make_exists.rb
+++ controls/make_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm make exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'make')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-make_relative_path = input('command_path', value: '/bin/make')
-make_installation_directory = command("hab pkg path #{plan_ident}")
-make_full_path = make_installation_directory.stdout.strip + "#{ make_relative_path}"
- 
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+command_relative_path = input('command_relative_path', value: '/bin/make')
+command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+
 control 'core-plans-make-exists' do
   impact 1.0
-  title 'binary should exist'
+  title 'Ensure make exists'
   desc '
-  '
-   describe file(make_full_path) do
+  Verify make by ensuring /bin/make exists'
+  
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  describe file(command_full_path) do
     it { should exist }
   end
 end
