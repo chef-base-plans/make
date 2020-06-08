0257bb8bc53957053ece4dd49ce2caa092bb5740
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 17:25:52 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_apis/build/status/chef-base-plans.make?branchName=master)](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_build/latest?definitionId=69&branchName=master)
+
 # make
 
 Make is a tool which controls the generation of executables and other non-source files of a program from the program's source files.
--- controls/make_works.rb
+++ controls/make_works.rb
@@ -1,40 +1,32 @@
 title 'Tests to confirm make works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'make')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-make_path = command("hab pkg path #{plan_ident}")
-make_pkg_ident = ((make_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
-control 'core-plans-make-works-001' do
+control 'core-plans-make-works' do
   impact 1.0
-  title 'should be installed'
+  title 'Ensure make works as expected'
   desc '
+  Verify make by ensuring (1) its installation directory exists; (2) that
+  it returns the expected version; (3) it runs successfully against a sample Makefile
   '
-  describe make_path do
+  
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
-end
-
-control 'core-plans-make-works-002' do
-  impact 1.0
-  title 'should be at correct version'
-  desc '
-  '
-  describe command("DEBUG=true; hab pkg exec #{make_pkg_ident} make --version") do
+  
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} make --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /GNU Make 4.2.1/ }
+    its('stdout') { should match /GNU Make #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
-end
 
-control 'core-plans-make-works-003' do
-  impact 1.0
-  title 'should act on a Makefile'
-  desc '
-  '
-  describe command("DEBUG=true; hab pkg exec #{make_pkg_ident} make --directory /hab/svc/make/config/fixtures/ ci-test") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} make --directory /hab/svc/make/config/fixtures/ ci-test") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /Make in CI/ }
