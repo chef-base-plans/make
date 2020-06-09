f41ad56d372be74ddc2a42b01678e3004963013f
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-09 11:11:33 +0100

    Ensure pkg_installation_directory is verified in each test before proceeding
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/make_exists.rb
+++ controls/make_exists.rb
@@ -2,9 +2,6 @@ title 'Tests to confirm make exists'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'make')
-plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
-command_relative_path = input('command_relative_path', value: '/bin/make')
-command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
 
 control 'core-plans-make-exists' do
   impact 1.0
@@ -12,11 +9,14 @@ control 'core-plans-make-exists' do
   desc '
   Verify make by ensuring /bin/make exists'
   
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
 
+  command_relative_path = input('command_relative_path', value: '/bin/make')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
   describe file(command_full_path) do
     it { should exist }
   end
